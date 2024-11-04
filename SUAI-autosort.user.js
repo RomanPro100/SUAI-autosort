1a8e2ee5be3ef94e40ae530d6fededfb4e0af236
RomanPro100 <99795209+RomanPro100@users.noreply.github.com>
2024-11-04 14:27:53 +0300

    Добавил загрузку скрипта сразу при открытии страницы
    До этого скрипт ждал загрузки страницы перед запуском. Теперь он будет запускаться сразу при переходе на страницу, не дожидаясь её загрузки.

--- SUAI-autosort.user.js
+++ SUAI-autosort.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        SUAI-autosort
-// @version     1.1
+// @version     1.2
 // @description Автоматически сортирует задания в ЛК ГУАП по возрастанию дедалйна
 // @match       https://pro.guap.ru/inside/student/tasks/
 // @icon        https://pro.guap.ru/favicon/favicon.ico
 // @grant       none
+// @run-at      document-start
 // @author      RomanPro100
 // @namespace   https://github.com/RomanPro100
 // @license     GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
